bb8c2d988bdf6cbbc66a0bd5cb8511fe0327284e
lele <112399050+lethicya75@users.noreply.github.com>
2024-08-20 08:55:41 -0300

    20/08

--- script.js
+++ script.js
@@ -6,38 +6,38 @@ const textoResultado = document.querySelector(".texto-resultado");
 
     const perguntas = [
         {
-            enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+            enunciado: "Lewis nao queria renunciar a presidencia, mesmo voce fazendo mais que ele sendo presidente o que voce faz?",
             alternativas: [
-                "Isso é assustador!",
-                "Isso é maravilhoso!"
+                "bate nele!",
+                "tenta descobrir algum segredo dele!"
             ]
         },
         {
-            enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+            enunciado: "Apos isso voce resolve ir beber no bar do gus, chegando la voce escuta um barulho vindo da moita o que voce faz?",
             alternativas: [
-                "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
+                "fica com medo, mas mesmo assim espia.",
+                "começa a gravar e vai olhar o que é."
             ]
         },
         {
-            enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?++        ",
+            enunciado: "quando voce olha dentro da moita voce ve algo horrivel, Lewis com a marnie o que voce faz?++        ",
             alternativas: [
-                "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+                "começa a rir desesperadamente e ameaça contar para todo mundo.",
+                "fala que vai contar para todos."
             ]
         },
         {
-            enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+            enunciado: "No outro dia, lewis vai te visitar e começa a implorar para voce nao contar para ninguem o que voce faz?",
             alternativas: [
-                "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                "Criar uma imagem utilizando um gerador de imagem de IA."
+                "pede para ele anunciar que voce vai ser o novo/a presidente.",
+                "pede para ele dinheiro e que ele renuncie a presidencia."
             ]
         },
         {
-            enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+            enunciado: "Apos isso lewis renunciou a presidencia, e voce virou o/a presidente o que voce faz com lewis agora? ",
             alternativas: [
-                "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+                "continuo ameaçando e zoando ele.",
+                "Deixa ele viver feliz."
             ]
         }
     ]
